723275f0745237e6e9b784b6a99db819dfbb9671
Michael Liu <mike666234@gmail.com>
2019-04-14 01:56:24 -0700

    Add CardActionArea, input, submit button

--- src/Components/ListingCard.js
+++ src/Components/ListingCard.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import './ListingCard.css';
 
 const styles = {
+    button: {
+        margin: 5,
+        flex: 1,
+    },
     card: {
         width: 250,
-        height: 275,
         marginLeft: 10,
         marginRight: 10,
         marginTop: 15,
         marginBottom: 15,
-        paddingTop: 15,
+        paddingTop: 0,
+        paddingBottom: 0,
         flex: 1,
     },
 };
 
 class ListingCard extends React.Component {
+
+    state = {
+        qty: null,
+    };
+
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.value });
+    };
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.qty);
+        event.preventDefault();
+    };
+
     pluralizeWord(word) {
         if(!word)
             return '';
@@ -35,25 +57,43 @@ class ListingCard extends React.Component {
         const { classes } = this.props;
         return (
             <Card className={classes.card}>
-                <CardMedia
-                    className="listingCardImg"
-                    image={`/img/${this.props.itemName.toLowerCase()}.png`}
-                    title="Apple"
-                />
-                <CardContent>
-                    <Typography variant="h6" className="listingCardDesc">
-                        {this.pluralizeWord(this.props.itemName)}
-                    </Typography>
-                    <Typography variant="h6" className="listingCardDesc">
-                        ${this.props.price}
-                    </Typography>
-                </CardContent>
-                <LinearProgress className="ProgressBar" value={this.props.percentComplete} valueBuffer={this.props.percentAdditional + this.props.percentComplete} variant="buffer" />
-                <CardContent>
-                    <Typography variant="h6" className="listingCardDesc">
-                        Time left: {this.props.timeLeft}
-                    </Typography>
-                </CardContent>
+                <CardActionArea>
+                    <CardMedia
+                        className="listingCardImg"
+                        image={`/img/${this.props.itemName.toLowerCase()}.png`}
+                        title="Apple"
+                    />
+                    <CardContent>
+                        <Typography variant="h6" className="listingCardDesc">
+                            {this.pluralizeWord(this.props.itemName)}
+                        </Typography>
+                        <Typography variant="h6" className="listingCardDesc">
+                            ${this.props.price}
+                        </Typography>
+                    </CardContent>
+                    <LinearProgress className="ProgressBar" value={this.props.percentComplete} valueBuffer={this.props.percentAdditional + this.props.percentComplete} variant="buffer" />
+                    <CardContent>
+                        <Typography variant="h6" className="listingCardDesc">
+                            Time left: {this.props.timeLeft}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <form onSubmit={this.handleSubmit}>
+                        <TextField
+                            id="standard-number"
+                            label="Quantity"
+                            value={this.state.qty}
+                            onChange={this.handleChange('qty')}
+                            type="number"
+                            className={classes.textField}
+                            margin="normal"
+                        />
+                        <Button variant="contained" color="primary" className={classes.button} type="submit">
+                            Buy
+                        </Button>
+                    </form>
+                </CardActions>
             </Card>
         );
     }
